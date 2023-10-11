3b1b9db401145cc7535a30d2d596c530a62fa601
RaulQuesadaSaiz <15586180.clot@fje.edu>
2023-10-11 20:34:36 +0200

    Commit 2 - grup01 - Activitat gp1f4a5

--- codi_projectes/ppv/app/ppv.php
+++ codi_projectes/ppv/app/ppv.php
@@ -4,22 +4,24 @@
         <meta charset="utf-8">
         <title>PRICE OF THE SELECTED MATCH</title>
 </head>
-        <body>
-                <p><u>PRICE OF THE SELECTED MATCH</u></p>
-                <?php
-			if ($_GET["e1"]==$_GET["e2"]){
-				echo "Wrong selection. A team can not play against itself. Select a different team 2, please</br>";
-			}
-			else{
-				if ((($_GET["e1"]=="a") and  ($_GET["e2"]=="b")) or (($_GET["e1"]=="b") and  ($_GET["e2"]=="a"))){
-					echo "Special match: Price: 15 Euros</br>";
-				}
-				else {
-					echo "Basic match. Price: 10 Euros</br>";
-				}
-			}
-		?>
-		<br>
-		<a href="index.html">Return to the init web page</a>
-        </body>
+<body>
+        <p><u>PRICE OF THE SELECTED MATCH</u></p>
+        <?php
+        if ($_GET["e1"]==$_GET["e2"]){
+                echo "Wrong selection. A team cannot play against itself. Select a different team 2, please</br>";
+        }
+        else{
+                // Modificamos la condición para el partido especial
+                if ((($_GET["e1"]=="Rocaparda Village Sporting Club") and  ($_GET["e2"]=="North Pallerola Athletic Club.")) or (($_GET["e1"]=="North Pallerola Athletic Club.") and  ($_GET["e2"]=="Rocaparda Village Sporting Club"))){
+                        // Cambiamos el precio del partido especial a 13 Euros
+                        echo "Special match: Price: 13 Euros</br>";
+                }
+                else {
+                        echo "Basic match. Price: 10 Euros</br>";
+                }
+        }
+        ?>
+        <br>
+        <a href="index.html">Return to the init web page</a>
+</body>
 </html>
